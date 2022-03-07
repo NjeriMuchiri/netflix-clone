45abf8618f78773e91323f8783570cdb199a591a
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-07 19:34:44 +0300

    created new features

---
+++ src/Row.js
@@ -0,0 +1,24 @@
+import React, {useState,useEffect} from 'react';
+import axios from './axios';
+
+function Row({title}) {
+    const[movies,setMovies] = useState([]);
+
+    useEffect(() =>{
+        
+        async function fetchData(){
+
+        }
+        fetchData();
+    },[])
+
+  return (
+    <div>
+       <h2>{title}</h2>
+
+        {/* container posters */}
+    </div>
+  )
+}
+
+export default Row;
