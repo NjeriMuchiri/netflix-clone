d7421db30e3d867755c22630ba5f52e090513d1b
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-08 11:19:36 +0300

    Added a new component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import Row from './Row.js';
 import requests from './requests';
+import Banner from './Banner'
 
 function App() {
   return (
     <div className="App">
+       <Banner/>
+
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
       isLargeRow
       />
